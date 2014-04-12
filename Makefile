053931cf113f69682bb3f5578c18dcdcc88e54ff
Bob Urberger <burberger@gmail.com>
2014-04-12 15:10:09 -0700

    Parses polish notation

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 all: blisp
 
-blisp: prompt.c
-	$(CC) -Wall -g -std=c99 -o blisp prompt.c
+blisp: prompt.c mpc.c mpc.h
+	$(CC) -Wall -g -std=c99 -o blisp prompt.c mpc.c -ledit -lm
--- prompt.c
+++ prompt.c
@@ -4,18 +4,53 @@
  * REPL system
  */
 #include <stdio.h>
+#include <stdlib.h>
 
-// Max input size of 2048
-static char input[2048];
+#include <editline/readline.h>
+
+#include "mpc.h"
 
 int main(int argc, char** argv) {
-    // Print version info
-    puts("BLisp v0.0.1");
-    puts("Press Ctrl+C to Exit");
+  /* Create parser */
+  mpc_parser_t* number   = mpc_new("number");
+  mpc_parser_t* operator = mpc_new("operator");
+  mpc_parser_t* expr     = mpc_new("expr");
+  mpc_parser_t* blisp    = mpc_new("blisp");
 
-    while (1) {
-        fputs("Blisp> ", stdout);
-        fgets(input, 2048, stdin);
-        printf("%s", input);
+  mpca_lang(MPC_LANG_DEFAULT,
+    "                                                    \
+      number   : /-?[0-9]+/ ;                            \
+      operator : '+' | '-' | '*' | '/' ;                 \
+      expr     : <number> | '(' <operator> <expr>+ ')' ; \
+      blisp    : /^/ <operator> <expr>+ /$/ ;            \
+    ",
+    number, operator, expr, blisp
+  );
+
+  // Print version info
+  puts("BLisp v0.0.1");
+  puts("Press Ctrl+C to Exit");
+
+  while (1) {
+    char* input = readline("Blisp> ");
+    add_history(input);
+
+    //Parse input
+    mpc_result_t r;
+    if (mpc_parse("<stdin>", input, blisp, &r)) {
+      //Parsed, print AST
+      mpc_ast_print(r.output);
+      mpc_ast_delete(r.output);
+    } else {
+      //Failed, print parser error
+      mpc_err_print(r.error);
+      mpc_err_delete(r.error);
     }
+
+    free(input);
+  }
+
+  //Cleanup parser before exiting
+  mpc_cleanup(4, number, operator, expr, blisp);
+  return 0;
 }
