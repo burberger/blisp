de08fd47705bf276c51843fe25006d63d10be614
Bob Urberger <burberger@gmail.com>
2014-04-27 23:57:32 -0700

    Cleanup of header file sepeartion

--- builtin.c
+++ builtin.c
@@ -1,27 +1,5 @@
 #include "builtin.h"
 
-//Macro for error checking in builtin functions
-#define LASSERT(args, cond, fmt, ...) \
-  if (!(cond)) { \
-    lval* err = lval_err(fmt, ##__VA_ARGS__); \
-    lval_del(args); \
-    return err; \
-  }
-
-#define LASSERT_TYPE(func, args, index, expect) \
-  LASSERT(args, args->cell[index]->type == expect, \
-      "Function '%s' passed incorrect type for argument %i. Got %s, Expected %s", \
-      func, index, ltype_name(args->cell[index]->type), ltype_name(expect))
-
-#define LASSERT_NUM(func, args, num) \
-  LASSERT(args, args->count == num, \
-      "Function %s passed incorrect number of arguments. Got %i, Expected %i.", \
-      func, args->count, num)
-
-#define LASSERT_NOT_EMPTY(func, args, index) \
-  LASSERT(args, args->cell[index]->count != 0, \
-      "Function %s passed {} for argument %i.", func, index)
-
 char* ltype_name(ltype_t type) {
   switch(type) {
     case LVAL_FUN: return "Function";
